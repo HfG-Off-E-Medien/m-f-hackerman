a92be76e61d3becff9ce3e549f5ca43b49ca830e
HfG-Off-E-Medien <131382691+HfG-Off-E-Medien@users.noreply.github.com>
2023-05-02 15:41:09 +0200

    Update echo_client.py

--- echo_client.py
+++ echo_client.py
@@ -1,29 +1,14 @@
 # echo-client.py
 
 import socket
-from os.path import getsize
+from tcp_funcs import *
 
 HOST = 
-PORT =
+PORT = 
 BUFFER_SIZE = 1024
-
-def send_file(server, BUFFER_SIZE, FILENAME):
-    filesize = getsize(FILENAME)
-    print("Sending file with following size: ", filesize)
-    server.send(f"{filesize}".encode())
-    with open(FILENAME, "rb") as f:
-        while True:
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                return True
-            server.sendall(bytes_read)
-        #progress.update(len(bytes_read))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    send_file(s, BUFFER_SIZE, "hello.txt")
-    """
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-    """
+    send_file(s, BUFFER_SIZE, "Rothwald.jpg")
+    _ = receive_file(s, BUFFER_SIZE, "server_answer.jpg")
