7eaaf1fcca3598a5ce95a13954dc1e29d6cbb69e
HfG-Off-E-Medien <131382691+HfG-Off-E-Medien@users.noreply.github.com>
2023-05-02 13:00:36 +0200

    Update echo_client.py

--- echo_client.py
+++ echo_client.py
@@ -1,13 +1,21 @@
 # echo-client.py
 
 import socket
+from os.path import getsize
 
 HOST = 
 PORT =  
 
+def send_file(server, FILENAME):
+    filesize = getsize(FILENAME)
+    print(filesize)
+    server.send(f"{filesize}".encode())
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+
+    send_file(s, "hello.txt")
+    """
     s.sendall(b"Hello, world")
     data = s.recv(1024)
-
-print(f"Received {data!r}")
+    """
