257d999284b1f7c03beda8d03a3fd89e7550995e
HfG-Off-E-Medien <131382691+HfG-Off-E-Medien@users.noreply.github.com>
2023-05-02 15:43:12 +0200

    Update tcp_funcs.py

--- tcp_funcs.py
+++ tcp_funcs.py
@@ -1,22 +1,27 @@
-import socket
 from os.path import getsize
 from tqdm import tqdm
 
-def send_file(client, filename, BUFFER_SIZE):
-    filesize = getsize(filename)
-    client.send(f"{filename}".encode())
-
-    # start sending the file
-    progress = tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
-    with open(filename, "rb") as f:
-        while True:
-            # read the bytes from the file
+def send_file(server, BUFFER_SIZE, FILENAME):
+    filesize = getsize(FILENAME)
+    print("Sending file with following size: ", filesize)
+    server.sendall(f"{filesize}".encode())
+    progress = tqdm(range(0, filesize, BUFFER_SIZE), f"Sending...", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
+    with open(FILENAME, "rb") as f:
+        for i in progress:
             bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                # file transmitting is done
-                break
-            # we use sendall to assure transimission in 
-            # busy networks
-            client.sendall(bytes_read)
-            # update the progress bar
+            server.sendall(bytes_read)
             progress.update(len(bytes_read))
+
+def receive_file(conn, BUFFER_SIZE, OUT_PATH):
+    """ Receive filesize from the client """
+    filesize = int(conn.recv(BUFFER_SIZE).decode())
+    print('File mit folgender Size angekuendigt: ', filesize)
+
+    """ Receive buffers """
+    progress = tqdm(range(0, filesize, BUFFER_SIZE), f"Receiving...", unit="B", unit_scale=True, unit_divisor=BUFFER_SIZE)
+    with open(OUT_PATH, "wb") as f:
+        for i in progress:
+            bytes_read = conn.recv(BUFFER_SIZE)
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+    return filesize
